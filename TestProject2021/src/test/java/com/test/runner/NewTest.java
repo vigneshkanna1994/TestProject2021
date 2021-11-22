abcd3aa3eaf35f3066309ff1f0a950edbb630de0
vigneshkanna <vigneshkanna1994@gmail.com>
2021-11-22 13:03:22 +0530

    updating the Testng file and added new test for demo

---
+++ TestProject2021/src/test/java/com/test/runner/NewTest.java
@@ -0,0 +1,22 @@
+package com.test.runner;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.err.println("Test method");
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Before method");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  System.out.println("After method");
+  }
+
+}
